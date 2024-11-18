776f4bffe4ad82e6d65d5743868ae647d664b375
whchzh1313@gmail.com <whchzh1313@gmail.com>
2024-11-18 20:06:20 +0900

    api키를 숨긴다.

--- inc/hd_console.php
+++ inc/hd_console.php
@@ -1,3 +1,22 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 ?>
+<div id="hdConsole">
+    <ul>
+        <li>
+            <h3>Place</h3>
+            <p></p>
+        </li>
+        <li>
+            <h3>Time</h3>
+            <p><?php echo date("H:i:s"); ?></p>
+        </li>
+        <li>
+            <h3>Weather</h3>
+            <p><img src="" alt=""></p>
+        </li>
+    </ul>
+</div>
+<div id="placeConsole"></div>
+<div id="timeConsole"></div>
+<div id="weatherConsole"></div>
