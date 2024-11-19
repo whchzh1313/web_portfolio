53a401378ffd93b572acab839e7baf5901094aa4
whchzh1313@gmail.com <whchzh1313@gmail.com>
2024-11-19 22:58:45 +0900

    javascript fadeIn, fadeout

--- inc/hd_console.php
+++ inc/hd_console.php
@@ -2,7 +2,8 @@
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 ?>
 <style>
-    #hdConsole {position: fixed;right: 24px;top: 112px;padding: 4px;background: #fff;box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);display: -webkit-box;display: -ms-flexbox;display: -webkit-flex;display: flex;border-radius: 4px;font-size: 1rem;overflow: hidden;}
+    .console_style {box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);position: fixed;right: 24px;border-radius: 4px;background: #fff;transition: opacity ease .5s;}
+    #hdConsole {top: 112px;padding: 4px;display: -webkit-box;display: -ms-flexbox;display: -webkit-flex;display: flex;font-size: 1rem;overflow: hidden;}
     #hdConsole.hide {width: fit-content;}
     #hdConsole #consolItems {width: 400px;transition:width ease 1s;}
     #hdConsole.hide #consolItems {width: 0;}
@@ -15,8 +16,12 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
     #hdConsole #consolItems > li > h3 + p {background: #E9EBF7;font-weight: 400;border-radius: 4px;padding: 0 8px;height: 28px;}
     #hdConsole #consolItems > li #weatherIcon {padding: 0;}
     #hdConsole #consolItems > li #weatherIcon img {height: 28px;}
+
+    #placeConsole {top: 184px;display: none;}
+    #timeConsole {top: 184px;display: none;}
+    #weatherConsole {top: 184px;display: none;}
 </style>
-<div id="hdConsole" class="">
+<div id="hdConsole" class="console_style">
     <div id="btConsoleToggle"><button type="button"><img src="/images/hd_console_btn.png" alt=""></button></div>
     <ul id="consolItems">
         <li>
@@ -33,16 +38,16 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
         </li>
     </ul>
 </div>
-<div id="placeConsole">
-    
+<div id="placeConsole" class="console_style">
+    1
     <div id="map"></div>
     <button class="close"></button>
 </div>
-<div id="timeConsole">
-
+<div id="timeConsole" class="console_style">
+    2
 </div>
-<div id="weatherConsole">
-
+<div id="weatherConsole" class="console_style">
+    3
 </div>
 <script>
     /* 현재 날짜, 시간 지정 */
@@ -72,22 +77,57 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
         _hdConsole.classList.toggle("hide");
     });
 
-
+    const _placeConsole = document.getElementById("placeConsole")
+    const _timeConsole = document.getElementById("timeConsole")
+    const _weatherConsole = document.getElementById("weatherConsole")
+    // TODO 클릭시 해당 기능 열리기 제작
     _hdConsole.querySelector("#placeName").addEventListener("click", function() {
-        document.getElementById("placeConsole").classList.toggle("active");
+        _placeConsole.classList.toggle("active");
+        if (_placeConsole.classList.contains('active')) {
+            fadeIn(_placeConsole);
+        } else {
+            fadeOut(_placeConsole);
+        }
     });
     _hdConsole.querySelector("#dateTime").addEventListener("click", function() {
-        document.getElementById("timeConsole").classList.toggle("active");
+        _timeConsole.classList.toggle("active");
+        if (_timeConsole.classList.contains('active')) {
+            fadeIn(_timeConsole);
+        } else {
+            fadeOut(_timeConsole);
+        }
     });
     _hdConsole.querySelector("#weatherIcon").addEventListener("click", function() {
-        document.getElementById("weatherConsole").classList.toggle("active");
+        _weatherConsole.classList.toggle("active");
+        if (_weatherConsole.classList.contains('active')) {
+            fadeIn(_weatherConsole);
+        } else {
+            fadeOut(_weatherConsole);
+        }
     });
 
-    
-    
-    
-
-    // TODO 클릭시 해당 기능 열리기 제작
+    function fadeAllOut () {
+        _placeConsole.style.opacity = 0; // 페이드 아웃 효과
+        _timeConsole.style.opacity = 0; // 페이드 아웃 효과
+        _weatherConsole.style.opacity = 0; // 페이드 아웃 효과
+        setTimeout(() => {
+            _placeConsole.style.display = 'none'; // 요소를 숨김
+            _timeConsole.style.display = 'none'; // 요소를 숨김
+            _weatherConsole.style.display = 'none'; // 요소를 숨김
+        }, 200); // transition 시간과 일치시킴
+    }
+    function fadeIn (selectElement) {
+        selectElement.style.display = 'block'; // 요소를 보이게 함
+        setTimeout(() => {
+            selectElement.style.opacity = 1; // 페이드 인 효과
+        }, 500); // display가 block으로 설정된 후 opacity를 변경
+    }
+    function fadeOut (selectElement) {
+        selectElement.style.opacity = 0; // 페이드 아웃 효과
+        setTimeout(() => {
+            selectElement.style.display = 'none'; // 요소를 숨김
+        }, 500); // transition 시간과 일치시킴
+    }
 
     /******* Clock ********/
     const  clockTarget = document.getElementById("dateTime");
