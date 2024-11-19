12eee60c26055e6423dd69026721ac9b3f50653f
whchzh1313@gmail.com <whchzh1313@gmail.com>
2024-11-19 13:06:17 +0900

    add:hd_console.php clock 기능 추가

--- inc/hd_console.php
+++ inc/hd_console.php
@@ -1,22 +1,93 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 ?>
-<div id="hdConsole">
-    <ul>
+<style>
+    #hdConsole {position: fixed;right: 24px;top: 112px;padding: 4px;background: #fff;box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);display: -webkit-box;display: -ms-flexbox;display: -webkit-flex;display: flex;border-radius: 4px;font-size: 1rem;overflow: hidden;}
+    #hdConsole.hide {width: fit-content;}
+    #hdConsole #consolItems {width: 400px;transition:width ease 1s;}
+    #hdConsole.hide #consolItems {width: 0;}
+    #hdConsole #btConsoleToggle {line-height: 28px;text-align: center;width: 28px;margin-right: 2px;}
+    #hdConsole #btConsoleToggle button {border: none;background: none;width: 100%;height: 100%;transform: rotate(180deg);}
+    #hdConsole.hide #btConsoleToggle button {transform: rotateX(180deg);}
+    #hdConsole #consolItems {display: -webkit-box;display: -ms-flexbox;display: -webkit-flex;display: flex;line-height: 28px;white-space: nowrap;}
+    #hdConsole #consolItems > li {display: -webkit-box;display: -ms-flexbox;display: -webkit-flex;display: flex;padding: 0 8px;}
+    #hdConsole #consolItems > li > h3 {margin-right: 8px;line-height: 28px;font-weight: 500;}
+    #hdConsole #consolItems > li > h3 + p {background: #E9EBF7;font-weight: 400;border-radius: 4px;padding: 0 8px;height: 28px;}
+    #hdConsole #consolItems > li #weatherIcon {padding: 0;}
+    #hdConsole #consolItems > li #weatherIcon img {height: 28px;}
+</style>
+<div id="hdConsole" class="">
+    <div id="btConsoleToggle"><button type="button"><img src="/images/hd_console_btn.png" alt=""></button></div>
+    <ul id="consolItems">
         <li>
             <h3>Place</h3>
-            <p></p>
+            <p id="placeName">성남시 분당구</p>
         </li>
         <li>
             <h3>Time</h3>
-            <p><?php echo date("H:i:s"); ?></p>
+            <p id="dateTime"></p>
         </li>
         <li>
             <h3>Weather</h3>
-            <p><img src="" alt=""></p>
+            <p id="weatherIcon"><img src="/images/icons/ic_cloudsun.png" alt=""></p>
         </li>
     </ul>
 </div>
 <div id="placeConsole"></div>
 <div id="timeConsole"></div>
 <div id="weatherConsole"></div>
+<script>
+    /* 현재 날짜, 시간 지정 */
+    // var clockTarget = document.getElementById("clock");
+    // function clock() {
+    //     var date = new Date();
+    //     var month = date.getMonth();
+    //     var clockDate = date.getDate();
+    //     var day = date.getDay();
+    //     var week = ['일', '월', '화', '수', '목', '금', '토'];
+    //     var hours = date.getHours();
+    //     var minutes = date.getMinutes();
+    //     var seconds = date.getSeconds();
+    //     clockTarget.innerText = `${month+1}월 ${clockDate}일 ${week[day]}요일` +
+    //     `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes }`  : minutes }:${seconds < 10 ? `0${seconds }`  : seconds }`;
+    // }
+    // function init() {
+    //     clock();
+    //     setInterval(clock, 1000);
+    // }
+    // init();
+    /* 갱신 시간 지정 */
+
+    /******* Clock ********/
+    const  clockTarget = document.getElementById("dateTime");
+    function clock() {
+        var date = new Date();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        clockTarget.innerText = (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes);
+    }
+
+    // 시계 작동 시작 ( 1초마다 반복 )
+    let runClock;
+    function startClock() {
+        clock();
+        runClock = setInterval(clock, 1000);
+    }
+
+    // TODO setInterval이 걸린 clock 함수를 멈출 코드
+    function stopClock() {
+        clearInterval(runClock);
+    }
+
+    // TODO 시간을 지정하고 그 시간에 맞춰 #main에 time_$ 클래스를 추가할 예정
+    function setClock(hour, minute) {
+        clockTarget.innerText = (hour < 10 ? '0' + hour : hour) + ':' + (minute < 10 ? '0' + minute : minute);
+    }
+
+    const _hdConsole = document.getElementById("hdConsole");
+    _hdConsole.querySelector("#btConsoleToggle").addEventListener("click", function() {
+        _hdConsole.classList.toggle("hide");
+    });
+
+    
+</script>
---
+++ index_console_test.php
@@ -0,0 +1,36 @@
+<?php
+include_once('./_common.php');
+
+define('_INDEX_', true);
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+if(defined('G5_THEME_PATH')) {
+    require_once(G5_THEME_PATH.'/index.php');
+    return;
+}
+
+if (G5_IS_MOBILE) {
+    include_once(G5_MOBILE_PATH.'/index.php');
+    return;
+}
+
+include_once(G5_PATH.'/head.php');
+add_javascript('<script src="'.G5_JS_URL.'/main.js"></script>', 0);
+include_once(G5_PATH.'/inc/hd_console.php');
+?>
+
+    <div id="main">
+        <div id="titleSection" class="blue_gradiant_bg center_y_title">
+            <div class="section wave_text" data-aos="fade">
+                <p class="words wave" aria-hidden="true">이화민<span class="hide_and_show">_</span><br />웹 개발자 포트폴리오</p>
+                <p class="words">이화민<span class="hide_and_show">_</span><br />웹 개발자 포트폴리오</p>
+            </div>
+        </div>
+        <div id="aboutMe" class="section">
+            
+        </div>
+        <style>
+        </style>
+    </div>
+<?php
+include_once(G5_PATH.'/tail.php');
